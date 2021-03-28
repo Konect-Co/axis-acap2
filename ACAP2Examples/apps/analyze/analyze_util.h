e4cfe7d0a653ce3b55eea8177f999a1c65629b33
Ravit Sharma <sharma.ravit@gmail.com>
2021-03-28 08:40:26 -0700

    Removing unused files

--- ACAP2Examples/apps/analyze/analyze_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ANALYZE_UTIL_H_
-#define _ANALYZE_UTIL_H_
-
-gboolean
-util_string_to_guint(const gchar *str,
-    guint *val);
-
-#endif /* _ANALYZE_UTIL_H_ */
--- cppTest/test.cpp
+++
@@ -1,9 +0,0 @@
-//#include <opencv2/opencv.hpp>
-#include <iostream>
-
-using namespace std;
-
-int main(){
-	cout << "Hello" << endl;
-	return 0;	
-}
