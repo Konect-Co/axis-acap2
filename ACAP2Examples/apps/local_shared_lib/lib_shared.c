a203b79c82c6b684dfb4548846dbfc8c2ad594fc
Ravit Sharma <sharma.ravit@gmail.com>
2021-03-28 08:40:26 -0700

    Removing unused files

--- ACAP2Examples/apps/local_shared_lib/lib_shared.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "lib_shared.h"
-
-int shared_lib_function(void)
-{
-  fprintf(stdout, "local_shared_lib: \n");
-  return SHARED_LIB_OK;
-}
-
--- ACAP2Examples/apps/local_shared_lib/lib_shared.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LIB_SHARED_H__
-#define __LIB_SHARED_H__
-
-#define SHARED_LIB_OK 0
-
-int shared_lib_function(void);
-
-#endif /* LIB_SHARED */
-
