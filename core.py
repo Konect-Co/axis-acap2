5864aff988582d9b989372a88d0e86d0b7e729f8
Santript Sharma <santript.sharma@gmail.com>
2021-04-02 12:02:45 -0700

    Tracking somewhat working (Problem: removes tracked objects since iou is lower than threshold and then creates new obj)

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import os
 import log
+import utils
 
 from cv_model import pred
 from TrackedObject import TrackedObject
@@ -32,13 +33,16 @@ def track(fps=10, duration=15, ip='10.0.0.148'):
 	tracker = cv2.TrackerCSRT_create()
 	output = pred.predict(frame)
 	detection_threshold = 0.6
-    iou_threshold = 0.7
-    score_add_threshold = 0.8
+	iou_threshold = 0.5
+	scaling = 6
+	#score_add_threshold = 0.6
 	trackingObjs = []
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+	fps = cap.get(cv2.CAP_PROP_FPS)
 	width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 	name = "video"
+	outfps = int(1.0*fps/scaling)
 	#orig = cv2.VideoWriter(name + "_orig.avi", fourcc, fps, (width,height))
 	out = cv2.VideoWriter(name + "_out.avi", fourcc, fps, (width,height))
 	if len(output["scores"]) == 0:
@@ -61,6 +65,7 @@ def track(fps=10, duration=15, ip='10.0.0.148'):
 		log.LOG_INFO("Bounding box: " + str(tuple(bbox)))
 		trackerObj = TrackedObject(cv2.TrackerCSRT_create())
 		bbox = (bbox[1], bbox[0], bbox[3]-bbox[1], bbox[2]-bbox[0])
+		trackerObj.updateBox(bbox)
 		trackerObj.tracker.init(frame, bbox)
 		trackingObjs.append(trackerObj)
 	frame_no = 1
@@ -72,6 +77,8 @@ def track(fps=10, duration=15, ip='10.0.0.148'):
 			break
 		for trackerObj in trackingObjs:
 			success, box = trackerObj.tracker.update(frame)
+			#print("Box: ", box)
+			trackerObj.updateBox(box)
 			if success:
 				(x, y, w, h) = [int(v) for v in box]
 				_ = cv2.rectangle(frame, (x, y), (x+w, y+h), trackerObj.color, 2)
@@ -80,54 +87,118 @@ def track(fps=10, duration=15, ip='10.0.0.148'):
 				deleteTrackedObjs.append(trackerObj)
 				#print("Failed at frame", frame_no)
 		frame_no += 1
+
 		out.write(frame)
 		for obj in deleteTrackedObjs:
 			trackingObjs.remove(obj)
-        
-        output = pred.predict(frame)
-        IOU_vals = {}
 
-        #making a dictionary of IOU_vals
-        #each (key, value) of the dictionary is (<uuid of tracked object to_curr>, <dictionary of IoUs>)
-        #each (key, value) of <dictionary of IoUs> is (<index of bounding box bb_curr>, <IoU of to_curr.box and bb_curr>)
-        for trackedObj in trackedObjects:
-            IOU_vals[trackedObj.uuid] = {i:computeIOU(trackedObj.bbox, output['boxes'][i]) for i in range(len(output['boxes']))}
+		if frame_no % scaling != 0:
+			continue
 
-        #setting up list of newTrackedObjs to add (starts off full, and boxes that match existing TrackedObjects are gradually removed)
-        #setting up list of trackedObjectsToDelete (starts off empty, trackedObjects with no corresponding bounding box are added)
-        #newTrackedObjs = list(range(<index of detections with score > score_add_threshold>) #TODO
-        trackedObjectsToDelete = []
-        for currTrackedObjUUID, trackedObjIoUs in IOU_vals:
-            #currTrackedObj = <find tracked object with specified uuid> #TODO
-            #maxBoxIndex, maxIoU = <find max IoU in trackedObjIoUs, the box index that corresponds to it> #TODO
-            #We have a match between currTrackedObject and maxBox, and can update the box value
-            if maxIoU > iou_threshold:
-                maxBox = output['boxes'][maxBoxIndex]
-                #convert maxBox to proper format
-                currTrackedObj.box = maxBox
-                for _, innerTrackedObjIoUs in IoU_vals:
-                    del innerTrackedObjIoUs[maxBoxIndex]
-                newTrackedObjs.remove(maxBoxIndex)
-            #No adequate bounding box is found for currTrackedObj, so it should be deleted
-            else:
-                trackedObjectsToDelete.append(currTrackedObj)
+		output = pred.predict(frame)
 
-        #removing every TrackedObject in trackedObjectsToDelete
-        for trackedObjectToDelete in trackedObjectsToDelete:
-            trackingObjs.remove(trackedObjectToDelete)
+		IOU_vals = {}
 
-        #adding new TrackedObject for every bounding box index in newTrackedObj
-        for newTrackedObj in newTrackedObjs:
-            bbox = output['boxes'][newTrackedObj]
-            trackerObj = TrackedObject(cv2.TrackerCSRT_create())
-            trackerObj.tracker.init(frame, bbox)
-            trackingObjs.append(trackerObj)
+		for index in range(len(output['boxes'])):
+			bbox_temp = [0, 0, 0, 0]
+			bbox_temp[0] = int(output['boxes'][index][0]*frame.shape[0])
+			bbox_temp[1] = int(output['boxes'][index][1]*frame.shape[1])
+			bbox_temp[2] = int(output['boxes'][index][2]*frame.shape[0])
+			bbox_temp[3] = int(output['boxes'][index][3]*frame.shape[1])
+			output['boxes'][index] = [bbox_temp[1], bbox_temp[0], bbox_temp[3]-bbox_temp[1], bbox_temp[2]-bbox_temp[0]]
 
+		newTrackedObjs = []
+		for score_index in range(len(output['scores'])):
+			if output['scores'][score_index] > detection_threshold:
+				newTrackedObjs.append(score_index)
 
-        #Santript, this is commented because addToDatabase is not finished/tested
-        #databaseUpdate.addToDatabase(trackingObjs)
+		#making a dictionary of IOU_vals
+		#each (key, value) of the dictionary is (<uuid of tracked object to_curr>, <dictionary of IoUs>)
+		#each (key, value) of <dictionary of IoUs> is (<index of bounding box bb_curr>, <IoU of to_curr.box and bb_curr>)
+		print("New tracked objects: ", newTrackedObjs)
+		for trackedObj in trackingObjs:
+			print("Boxes: ")
+			print(output['boxes'])
+			IOU_vals[trackedObj.uuid] = {i:utils.computeIOU(list(trackedObj.bbox), output['boxes'][i]) for i in newTrackedObjs} if newTrackedObjs else None
 
-    cap.release()
+		if(not newTrackedObjs):
+
+			continue
+		print(IOU_vals)
+
+		#setting up list of newTrackedObjs to add (starts off full, and boxes that match existing TrackedObjects are gradually removed)
+		#setting up list of trackedObjectsToDelete (starts off empty, trackedObjects with no corresponding bounding box are added)
+		#newTrackedObjs = list(range(<index of detections with score > score_add_threshold>) #TODO
+
+		trackedObjectsToDelete = []
+		currTrackedObj = None
+
+		for currTrackedObjUUID, trackedObjIoUs in IOU_vals.items():
+			#currTrackedObj = <find tracked object with specified uuid> #TODO
+			for trackedObj in trackingObjs:
+				if trackedObj.uuid == currTrackedObjUUID:
+					currTrackedObj = trackedObj
+					
+			#maxBoxIndex, maxIoU = <find max IoU in trackedObjIoUs, the box index that corresponds to it> #TODO
+			maxIoU = trackedObjIoUs[list(trackedObjIoUs.keys())[0]] if len(trackedObjIoUs) != 0 else None
+			if maxIoU != None:
+				maxBoxIndex = list(IOU_vals[currTrackedObjUUID].keys())[0]
+				for index in list(IOU_vals[currTrackedObjUUID].keys()):
+					if IOU_vals[currTrackedObjUUID][index] > maxIoU:
+						maxIoU = IOU_vals[currTrackedObjUUID][index]
+						maxBoxIndex = index
+				#print("maxBoxIndex: ", maxBoxIndex)
+				#print()
+				print("uuid: ",currTrackedObj.uuid)
+				print("bounding box: ", currTrackedObj.bbox)
+				print("Maximum IOU: ", maxIoU)
+				print("Max box index: ", maxBoxIndex)
+				print("new tracked objects: ", newTrackedObjs)
+				#We have a match between currTrackedObject and maxBox, and can update the box value
+				if maxIoU > iou_threshold:
+					maxBox = output['boxes'][maxBoxIndex]
+					#convert maxBox to proper format
+					#currTrackedObj.bbox = maxBox
+					currTrackedObj.updateBox(maxBox)
+
+					#print("innerTrackedObjIoUs", innerTrackedObjIoUs)
+					#print("UUID: ", currTrackedObjUUID)
+					#print()
+
+					print(IOU_vals)
+					for uuid in list(IOU_vals.keys()):
+						del IOU_vals[uuid][maxBoxIndex]
+
+					#print(newTrackedObjs)
+						
+					newTrackedObjs.remove(maxBoxIndex)
+				#No adequate bounding box is found for currTrackedObj, so it should be deleted
+				else:
+					print(currTrackedObj.uuid)
+					trackedObjectsToDelete.append(currTrackedObj)
+
+		#removing every TrackedObject in trackedObjectsToDelete
+		for trackedObjectToDelete in trackedObjectsToDelete:
+			trackingObjs.remove(trackedObjectToDelete)
+
+		#adding new TrackedObject for every bounding box index in newTrackedObj
+		print("Size of newTrackedObjs: ", len(newTrackedObjs))
+		for newTrackedObj in newTrackedObjs:
+			print("New Tracked Object: ", newTrackedObj)
+			bbox = output['boxes'][newTrackedObj]
+			trackerObj = TrackedObject(cv2.TrackerCSRT_create())
+			trackerObj.updateBox(bbox)
+			trackerObj.tracker.init(frame, tuple(bbox))
+			trackingObjs.append(trackerObj)
+
+		print()
+		log.LOG_INFO("Next Frame", frame_no)
+		print()
+
+		#Santript, this is commented because addToDatabase is not finished/tested
+		#databaseUpdate.addToDatabase(trackingObjs)
+
+	cap.release()
 	out.release()
 	#orig.release()
 
---
+++ utils.py
@@ -0,0 +1,47 @@
+#box format: [x, y, height, width] where (x,y) is the top left corner
+
+#this function computes the area of a box
+def computeArea(box):
+	box_reformed = [box[0], box[1], box[0]+box[2], box[1]+box[3]]
+	return (box_reformed[2]-box_reformed[0])*(box_reformed[3]-box_reformed[1])
+
+#this function computes the intersection between two boxes
+def computeIntersection(boxA, boxB):
+	boxA_reformed = [boxA[0], boxA[1], boxA[0]+boxA[2], boxA[1]+boxA[3]]
+	boxB_reformed = [boxB[0], boxB[1], boxB[0]+boxB[2], boxB[1]+boxB[3]]
+
+	#cases to return 0
+	if (boxB_reformed[0]>boxA_reformed[2] or boxA_reformed[0]>boxB_reformed[2] or boxB_reformed[1]>boxA_reformed[3] or boxA_reformed[1]>boxB_reformed[3]):
+		return 0
+
+	#determining the X and Y values
+	xVals = [boxA_reformed[0], boxA_reformed[2], boxB_reformed[0], boxB_reformed[2]]
+	yVals = [boxA_reformed[1], boxA_reformed[3], boxB_reformed[1], boxB_reformed[3]]
+	#arranging from least to greatest
+	xVals.sort()
+	yVals.sort()
+	
+	#returning intersection
+	intersection = (xVals[2]-xVals[1])*(yVals[2]-yVals[1])
+	return intersection
+
+#this function computes the IoU between two boxes
+def computeIOU (boxA, boxB):
+
+	#computing intersection from above method
+	intersection = computeIntersection(boxA, boxB)
+	#computing area from above method
+	union = computeArea(boxA) + computeArea(boxB) - intersection
+	iou = intersection/union
+
+	#returning IOU
+	return iou
+
+#this function computes Intersection over Area of A
+def computeIOA (boxA, boxB):
+	intersection = computeIntersection(boxA, boxB)
+	aArea = computeArea(boxA)
+	ioa = intersection/aArea
+
+	#returning IOA
+	return ioa
