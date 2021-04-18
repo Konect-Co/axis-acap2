d9cd8142392ce7e8b56f8670de5ad6f5b4207b5d
Ravit Sharma <sharma.ravit@gmail.com>
2021-04-18 16:33:07 -0700

    First version of analytics working.

--- analyticsGen/analytics.py
+++ analyticsGen/analytics.py
@@ -32,13 +32,49 @@ my_database = mysql.connector.connect(
     password=databasePassword,
     database=database
 )
+cursor = my_database.cursor()
+
+def computeIntersectionality(recentRows):
+    #gender, race, age table indicating number of people in past specified amount of time
+    intersectionality = np.zeros((2, 5, 5))
+    for row in recentRows:
+        tracking_id, start_time, end_time, active, race, gender, age = row
+        
+        if (race is None or gender is None or age is None):
+            continue
+
+        gender_category = gender_categories.index(gender)
+        race_category = race_categories.index(race)
+        age_category = 0
+       
+        for i in range(1, len(age_categories)):
+            if (age_category >= age_categories[i]):
+                age_category = age_categories[i]
+            else:
+                break
+
+        intersectionality[gender_category][race_category][age_category] += 1
+    return intersectionality 
 
 def genAnalytics():
     outputFile = getOutputFilename()
-    #total number people seen every 5 (make 5 a variable?) minutes
-    NumPeople = [] 
-    #gender, race, age table indicating number of people in past specified amount of time
-    intersectionality = np.zeros((2, 5, 5))
+    
+    analytics = {}
+
+    rowQuery = "select * from cameraRecords where start_time >= Date_sub(now(), interval " + str(frequency) + " hour);"
+    cursor.execute(rowQuery)
+    recentRows = cursor.fetchall()
+    log.LOG_INFO("Obtained records from past " + str(frequency) + " hour(s)")
+    
+    analytics["numPeople"] = len(recentRows)
+    analytics["intersectionality"] = computeIntersectionality(recentRows).tolist()
+    
+    log.LOG_INFO("Finished computing analytics. Writing to output file.")
+
     with open(outputFile, 'w') as f:
-        #json.dump()
-        print("temp")
+        json.dump(analytics, f, indent=2)
+        foo = 1
+    log.LOG_INFO("Written to output file", outputFile)
+
+if (__name__ == '__main__'):
+    genAnalytics()
